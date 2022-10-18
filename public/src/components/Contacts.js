73f1e8a6c04e062a767c0dda409f20efa02d52e8
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-10-18 21:10:29 +0545

    Contact Update

--- public/src/components/Contacts.js
+++ public/src/components/Contacts.js
@@ -4,20 +4,51 @@ import Logo from "../assets/logo.jpg";
 
 
 export const Contacts = ({ contacts, currentUser }) => {
-    const [currentUserName , setCurrentUserName]= useState(undefined);
-    const [currentUserImage , setCurrentUserImage]= useState(undefined);
-    const [currentSelected , serCurrentSelected]=useState(undefined);
-    useEffect(()=>{
-        if(currentUser){
-        setCurrentUserImage(currentUser.avatarImage);
-        setCurrentUserName(currentUser.username);
+    const [currentUserName, setCurrentUserName] = useState(undefined);
+    const [currentUserImage, setCurrentUserImage] = useState(undefined);
+    const [currentSelected, serCurrentSelected] = useState(undefined);
+    useEffect(() => {
+        if (currentUser) {
+            setCurrentUserImage(currentUser.avatarImage);
+            setCurrentUserName(currentUser.username);
         }
-    },[currentUser])
-    const changeCurrentChat=(index , contact)=>{
+    }, [currentUser])
+    const changeCurrentChat = (index, contact) => {
 
+        return (<>
+            <div>
+                {
+                    currentUserImage && currentUserName && (
+                        <Container>
+                            <div className="brand">
+                                <img src={Logo} alt="logo" />
+                                <h3> Essenger</h3>
+
+                            </div>
+                            <div className="contacts">
+                                {
+                                    contacts.map((contact , index )=>{
+                                        return(
+                                            <div className="contact" key={index}>
+
+                                                </div>
+                                        )
+
+                                    })
+                                }
+                                                                                                                                                                                                                                                                                                                                                                                             
+
+                            </div>
+                        </Container>
+
+                    )
+                }
+
+            </div></>)
     }
-    return (<>
-        <div>
-        
-        </div></>)
 }
+
+const Container = styled.div`
+.brand{
+    color: white;
+}`;
